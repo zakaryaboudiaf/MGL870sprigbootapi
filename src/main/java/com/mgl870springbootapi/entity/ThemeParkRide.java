1ceb903a084f439dd98f3dd240ca6ed2f701ed59
zakaryanabil <zakarya.boudiaf.igee@gmail.com>
2024-03-31 20:02:51 -0400

    first commit

---
+++ src/main/java/com/mgl870springbootapi/entity/ThemeParkRide.java
@@ -0,0 +1,37 @@
+package com.mgl870springbootapi.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+
+
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+public class ThemeParkRide {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
+    private int thrillFactor;
+    private int vomitFactor;
+
+    public ThemeParkRide(String name, String description, int thrillFactor, int vomitFactor) {
+        this.name = name;
+        this.description = description;
+        this.thrillFactor = thrillFactor;
+        this.vomitFactor = vomitFactor;
+    }
+
+}
---
+++ src/main/java/com/mgl870springbootapi/repository/ThemeParkRideRepository.java
@@ -0,0 +1,14 @@
+package com.mgl870springbootapi.repository;
+
+import com.mgl870springbootapi.entity.ThemeParkRide;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
